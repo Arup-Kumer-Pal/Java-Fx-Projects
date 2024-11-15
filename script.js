6dbf790ff08d3333a347161569ddde4b16945c68
Arup Kumer Pal <aruppalmac@gmail.com>
2024-11-15 13:40:33 +0600

    Weather

--- script.js
+++ script.js
@@ -1,77 +1,55 @@
-var map = L.map('map').setView([23.702965, 90.451160], 10);
-var markerModeEnabled = false;
-var markers = [];
-
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZnHfBprMGZAWBgxNny1R',{
-    tileSize: 512,
-    zoomOffset: -1,
-    minZoom: 1,
-    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-    crossOrigin: true
-}).addTo(map);
-
-
-
-function getInfo() {
-    $.getJSON("get_info.php", function (data) {
-        console.log(data); // Add this line to log the fetched data
-        var dataText = "";
-        for (var i = 0; i < data.length; i++) {
-            dataText += "Name: " + data[i].name + ", Phone: " + data[i].phone + "\n";
+let weather = {
+  apiKey: "aa5d0ae1208e05710602cdd0df5ca10e",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
         }
-        document.getElementById("dataTextArea").value = dataText;
-    });
-}
-
-// Function to display data in text field
-function showData(data) {
-    // Assuming you have an input element with id "dataTextField"
-    var textField = document.getElementById("dataTextField");
-    textField.value = data[0].name + ": " + data[0].phone; // Displaying the first item in the array as an example
-}
-
-
-
-
-
-
-
-
-// Function to add markers for existing coordinates
-function addExistingMarkers(coordinates) {
-    coordinates.forEach(function(coord) {
-        var marker = L.marker([coord.lat, coord.lng]).addTo(map);
-        marker.bindTooltip(coord.label).openTooltip(); // Bind label as tooltip
-        markers.push(marker);
-    });
-}
-
-// AJAX request to retrieve existing coordinates from database
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var coordinates = JSON.parse(this.responseText);
-        addExistingMarkers(coordinates);
-    }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
-xmlhttp.open("GET", "get_coordinates.php", true);
-xmlhttp.send();
 
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
 
-function onMapClick(e) {
-    if (markerModeEnabled) {
-        var clickedLatLng = e.latlng;
-        addMarker(clickedLatLng);
-
-        // Send latitude, longitude to PHP script using AJAX
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("POST", "store_coordinates.php", true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("lat=" + clickedLatLng.lat + "&lng=" + clickedLatLng.lng);
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
     }
-}
+  });
 
-map.on('click', onMapClick);
-
-
+weather.fetchWeather("Dhaka");
 
